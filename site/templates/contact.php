a0ad6c39304d800858e2dc95e34e188bbc8b8c5c
JJ <jadeandpearls@gmail.com>
2025-05-23 15:02:16 -0600

    contact page!

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -6,50 +6,66 @@
   </div>
   </div>
   <div class="right-col">
-  <div class="mobile-only">
-    <h2><?= $page->template()->name() ?></h2>
-  </div>
-  <main class="main">
-      <?php if($success): ?>
-      <div class="alert success">
-          <p><?= $success ?></p>
-      </div>
-      <?php else: ?>
-      <?php if (isset($alert['error'])): ?>
-          <div><?= $alert['error'] ?></div>
-      <?php endif ?>
-      <form method="post" action="<?= $page->url() ?>">
-          <div class="honeypot">
+    <div class="mobile-only">
+      <h2><?= $page->template()->name() ?></h2>
+    </div>
+    <main class="main">
+      <?php if($page->message()->isNotEmpty()): ?> 
+          <div class="alert success">
+            <p><?= $page->message() ?></p>
+          </div>  
+          <a class="crumb" href="<?= url('about/contact')?>"><span class="back-arrow">←</span> Back to form </a>
+      <?php else: ?>  
+        <?php if (isset($alert['error'])): ?>
+            <div><?= $alert['error'] ?></div>
+        <?php endif ?>
+          <form action="https://api.web3forms.com/submit" method="POST" id="contact">
+
+          <!-- Replace with your Access Key -->
+          <input type="hidden" name="access_key" value="<?= $page->secret() ?>">
+
+          <!-- Subject Line -->
+          <input type="hidden" name="subject" id="subject" value="<?= $page->subject()?>">
+          <input type="hidden" name="from_name" value="yayjayfray website form">
+
+          <!-- Form Inputs -->
+          <div class="field">
+            <label for="name">Name <abbr title="required">*</abbr></label>
+            <input type="text" name="name" id="name" required>
+
+            <!-- <input value="<?= esc($data['name'] ?? '', 'attr') ?>"> -->
+            <!-- <?= isset($alert['name']) ? '<span class="alert error">' . esc($alert['name']) . '</span>' : '' ?> -->
+          </div>
+          <div class="field">
+            <label for="email">Email <abbr title="required">*</abbr></label>
+            <input type="email" name="email" id="email" required>
+            <!-- <input value="<?= esc($data['email'] ?? '', 'attr') ?>" required>
+            <?= isset($alert['email']) ? '<span class="alert error">' . esc($alert['email']) . '</span>' : '' ?> -->
+          </div>
+          <div class="field">
+            <label for="text">
+                Message <abbr title="required">*</abbr>
+            </label>
+            <textarea name="message" required></textarea>
+            <!-- <textarea id="text" name="text" required>
+                <?= esc($data['text'] ?? '') ?>
+            </textarea>
+            <?= isset($alert['text']) ? '<span class="alert error">' . esc($alert['text']) . '</span>' : '' ?> -->
+          </div>
+
+            <!-- <div class="honeypot">
               <label for="website">Website <abbr title="required">*</abbr></label>
               <input type="url" id="website" name="website" tabindex="-1">
-          </div>
-          <div class="field">
-              <label for="name">
-                  Name <abbr title="required">*</abbr>
-              </label>
-              <input type="text" id="name" name="name" value="<?= esc($data['name'] ?? '', 'attr') ?>" required>
-              <?= isset($alert['name']) ? '<span class="alert error">' . esc($alert['name']) . '</span>' : '' ?>
-          </div>
-          <div class="field">
-              <label for="email">
-                  Email <abbr title="required">*</abbr>
-              </label>
-              <input type="email" id="email" name="email" value="<?= esc($data['email'] ?? '', 'attr') ?>" required>
-              <?= isset($alert['email']) ? '<span class="alert error">' . esc($alert['email']) . '</span>' : '' ?>
-          </div>
-          <div class="field">
-              <label for="text">
-                  Message <abbr title="required">*</abbr>
-              </label>
-              <textarea id="text" name="text" required>
-                  <?= esc($data['text'] ?? '') ?>
-              </textarea>
-              <?= isset($alert['text']) ? '<span class="alert error">' . esc($alert['text']) . '</span>' : '' ?>
-          </div>
-          <input class="submit" type="submit" name="submit" value="Submit">
-      </form>
+            </div> -->
+
+            <input type="checkbox" name="botcheck" class="hidden" style="display: none;">
+
+            <input type="hidden" name="redirect" value="<?= url('contact/thanks')?>/">   <!-- success page -->
+
+            <button id="send" type="submit" class="button">Send Message</button>
+        </form>
       <?php endif ?>
-  </main>
+    </main>
   <div>
 </div>
 <?php snippet('footer') ?>
---
+++ site/templates/thanks.php
@@ -0,0 +1,22 @@
+<?php snippet('nav') ?>
+<div class="content contact">
+  <div class="left-col checkers">
+  <div class="msg">
+    <?= $page->parent()->inquiry()->kirbytext() ?>
+  </div>
+  </div>
+  <div class="right-col">
+    <div class="mobile-only">
+      <h2><?= $page->template()->name() ?></h2>
+    </div>
+    <main class="main">
+      <div class="alert success">
+        <p>Your message was sent!</p>
+        <p><?= $page->message() ?></p>
+        </div>  
+        <a class="crumb" href="<?= url('contact')?>"><span class="back-arrow">←</span> Back to form </a>
+      </div>
+    </main>
+  <div>
+</div>
+<?php snippet('footer') ?>
