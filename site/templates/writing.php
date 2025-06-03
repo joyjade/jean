1360c48689b858e4ea8b5f0d1e91fa3238d8a951
JJ <jadeandpearls@gmail.com>
2025-06-03 16:18:52 -0600

    remove old code

--- site/templates/writing.php
+++ site/templates/writing.php
@@ -6,24 +6,21 @@
 <div class="right-col">
   <h2 class="mobile-only">Other Writing</h2>
   <?php foreach ($genres as $genre): ?>
-    <?php if($genre != "Other"): ?>
-      <ul class="flex writing">
-        <?php if($page->$genre()->isNotEmpty()) :?>
-          <h3><?= $genre ?></h3>
-        <?php foreach(  $page->$genre() -> toStructure() as $item ): ?> 
-          
-          <li>
-            <a href="<?=$item -> link() ?>" target="_blank">
-              <p>“<span class="link_title"><?=$item -> title()?></span>,”
-              <span class="publisher"><?=$item -> publisher() ?></span>
-              (<?=$item -> date() ?>)</p>
-            </a>
-          </li>
-          <?php endforeach ?>
-          <?php endif ?>
-        </ul>
-        <?php endif ?>
+    <ul class="flex writing">
+      <?php if($page->$genre()->isNotEmpty()) :?>
+        <h3><?= $genre ?></h3>
+      <?php foreach($page->$genre() -> toStructure() as $item ): ?> 
+        <li>
+          <a href="<?=$item -> link() ?>" target="_blank">
+            <p>“<span class="link_title"><?=$item -> title()?></span>,”
+            <span class="publisher"><?=$item -> publisher() ?></span>
+            (<?=$item -> date() ?>)</p>
+          </a>
+        </li>
         <?php endforeach ?>
+        <?php endif ?>
+      </ul>
+    <?php endforeach ?>
 </div>
         
 <?php snippet('footer') ?>
